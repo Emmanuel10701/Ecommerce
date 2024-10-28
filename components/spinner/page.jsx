6fdd43de74cc68852d1796c5995b37ad6bed1d4d
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-10-28 12:05:03 +0300

    Convert LoadingSpinner from TypeScript to JavaScript �

--- components/spinner/page.jsx
+++ components/spinner/page.jsx
@@ -1,7 +1,8 @@
+// src/components/spinner/LoadingSpinner.js
 import React from 'react';
 import { PulseLoader } from 'react-spinners';
 
-const LoadingSpinner: React.FC = () => (
+const LoadingSpinner = () => (
   <div className="flex justify-center items-center h-screen">
     <PulseLoader color="#36d7b7" />
   </div>
